2ba1d1884432ddb2f179d87b5d6e3dcfc9be9f6a
Arjun <arjun_deshmukh_aiml@moderncoe.edu.in>
2024-10-16 17:02:31 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineCongif } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    plugins: [react()]
+    plugins: [react()],
+    base: '/Portfolio/'
 })
